f5c2566289b2d0709c5e7b09f7a35c63d43c90ab
AryamanChandra10 <82165466+AryamanChandra10@users.noreply.github.com>
2021-04-28 13:51:12 +0530

    making website responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+const responsive = {
+    
+    0: {
+        items: 1
+    },
+
+    320:{
+        items: 1
+    },
+    560:{
+        items: 2
+    },
+
+    960:{
+        items: 3
+    }
+}
+
 $(document).ready(function(){
 
     $nav = $('.nav');
@@ -7,16 +25,17 @@ $(document).ready(function(){
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse')
     })
-})
 
-    // owl-carousek for blog
+
+    // owl-carousel for blog
     $('.owl-carousel').owlCarousel({
         loop:true,
         autoplay:true,
         autoplayTimeout: 5000,
         dots: false,
         nav:true,
-        navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next'),]
+        navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next'),],
+        responsive: responsive
     })
 
     // dark mode
@@ -28,3 +47,12 @@ $(document).ready(function(){
         document.body.classList.toggle('light');
     });
     
+
+     // click to scroll top
+     $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+});
